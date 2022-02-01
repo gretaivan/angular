89d300aad2f809729ad11d5cb6ba851a6c9f7fff
gretaivan <greta.ivan91@gmail.com>
2022-02-01 16:29:23 +0000

    adds event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `Auto generated component reusing the existing: <app-server></app-server>
-  <button class="btn btn-primary" [disabled]="!allowNewServer" >Add Server</button>`,
+  <button class="btn btn-primary" [disabled]="!allowNewServer" (click)="onCreateServer()">Add Server</button>
+  <p>{{ serverCreationStatus }}</p>`,
   styleUrls: ['./servers.component.css']
 })
 
@@ -11,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server was created'; 
 
   constructor() { 
     setTimeout(() => {
@@ -19,6 +21,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(){
+  }
+
+  onCreateServer () {
+    this.serverCreationStatus = 'Server was created!'; 
   }
 
 }
