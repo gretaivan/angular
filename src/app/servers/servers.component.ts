e6fffc2ad43a59f6d2e4c2693cf896cb1a983b29
gretaivan <greta.ivan91@gmail.com>
2022-02-01 16:46:02 +0000

    adds two-way binding example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `Auto generated component reusing the existing: <app-server></app-server>
+
+  <label>Server Name</label>
+  <input type="text" id="name" class="form-control" [(ngModel)]="serverName">
   <button class="btn btn-primary" [disabled]="!allowNewServer" (click)="onCreateServer()">Add Server</button>
+  <p>Input value for: {{serverName}}</p>
   <p>{{ serverCreationStatus }}</p>`,
   styleUrls: ['./servers.component.css']
 })
@@ -13,6 +17,8 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created'; 
+  serverName ='';
+
 
   constructor() { 
     setTimeout(() => {
@@ -24,7 +30,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer () {
-    this.serverCreationStatus = 'Server was created!'; 
+    this.serverCreationStatus = 'Server ' + this.serverName + ' was created!'; 
+  }
+
+  onUpdateServerName (event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
