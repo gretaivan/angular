92c70c6f60857de3dd095eaf4dfb2d32544c444b
gretaivan <greta.ivan91@gmail.com>
2022-02-01 16:21:20 +0000

    adds property biding example using add server button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `Auto generated component reusing the existing: <app-server></app-server>
-  <button class="btn btn-primary">Add the Server</button>`,
+  <button class="btn btn-primary" [disabled]="!allowNewServer" >Add Server</button>`,
   styleUrls: ['./servers.component.css']
 })
+
+
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
 
-  ngOnInit(): void {
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(){
   }
 
 }
